8d3a092d8c5143a13fef0a3a0bc829e254718d1e
CrioUser <crio-user@criodo.com>
2022-07-21 11:46:24 +0530

    ME_GEEKTRUST_BACKEND

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Use below Command when Coding in Crio Workspace. Add comment for below command during code submission on Geektrust Platform
-# ./gradlew clean build -x test --no-daemon
+#./gradlew clean build -x test --no-daemon
+#java -jar /tmp/gradle_builds/backend/backend/libs/geektrust.jar sample_input/input1.txt
 
-#Remove the below comment during code submission on Geektrust Platform
-gradle clean build -x test --no-daemon
-
-java -jar build/libs/geektrust.jar sample_input/input1.txt
+# Remove the below comment during code submission on Geektrust Platform
+ gradle clean build -x test --no-daemon
+ java -jar build/libs/geektrust.jar sample_input/input1.txt
--- src/main/java/com/geektrust/backend/App.java
+++ src/main/java/com/geektrust/backend/App.java
@@ -18,7 +18,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+
 public class App {
     // To run the application ./gradlew run --args="input.txt"
    
